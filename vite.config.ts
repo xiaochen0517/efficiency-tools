3a2a7e90730871bffc6776d73c958374bf015895
mochenya <2827075398@qq.com>
2025-01-18 10:33:17 +0800

    add tailwindcss and shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from "vite";
+// @ts-ignore
+import path from "path";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -19,10 +26,10 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
       : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`

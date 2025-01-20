231c5f934e04f5cb50252525775dbf771e3b376b
mochenya <2827075398@qq.com>
2025-01-20 17:41:06 +0800

    添加窗口记忆功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 import {Button} from "@/components/ui/button.tsx";
+import {useEffect} from "react";
 
 function App() {
+
+  useEffect(() => {
+    // 启动时默认将窗口调整到页面中心
+    window.moveTo((window.screen.width - window.outerWidth) / 2, (window.screen.height - window.outerHeight) / 2);
+  }, []);
 
   return (
     <Button variant="default">Test</Button>
